d443bf361f72cd10ba29f552725085493d7f3095
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-25 22:27:38 +0100

    insert many tasks + users

--- mongodb.js
+++ mongodb.js
@@ -13,40 +13,40 @@ async function connectToDatabase() {
 
     // const result = await db.collection('users').insertMany([
     //   {
-    //     name: 'Pearl',
-    //     age: 87,
+    //     name: 'Sidney',
+    //     age: 17,
     //   },
     //   {
-    //     name: 'Maxine',
-    //     age: 22,
+    //     name: 'Laurie',
+    //     age: 18,
     //   },
     // ]);
 
     // console.log('Documents inserted: ', result);
 
-    const result = await db.collection('users').insertOne({
-      name: 'Nancy',
-      age: 17,
-    });
-    console.log('Document inserted with _id:', result.insertedId);
+    // const result = await db.collection('users').insertOne({
+    //   name: 'Nancy',
+    //   age: 17,
+    // });
+    // console.log('Document inserted with _id:', result.insertedId);
 
-    // const result = await db.collection('tasks').insertMany([
-    //   {
-    //     description: 'do dishes',
-    //     completed: false,
-    //   },
-    //   {
-    //     description: 'grocery shopping',
-    //     completed: true,
-    //   },
-    //   {
-    //     description: 'go to post office',
-    //     completed: false,
-    //   },
-    // ]);
-    // console.log('Documents inserted: ', result);
+    const result = await db.collection('tasks').insertMany([
+      {
+        description: 'pay utility bills',
+        completed: false,
+      },
+      {
+        description: 'pay rent',
+        completed: true,
+      },
+      {
+        description: 'go to pharmacy',
+        completed: false,
+      },
+    ]);
+    console.log('Documents inserted: ', result);
 
-    // await client.close();
+    await client.close();
   } catch (error) {
     console.error('Unable to connect to database:', error.message);
   }
